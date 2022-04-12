72e69d3f0dbb6a27208d92c4d10205edc4450fba
Kaja Sherief <101790522+kajasherif@users.noreply.github.com>
2022-04-12 16:23:57 +0530

    color picker code

---
+++ ColorPicker/colorPicker.js
@@ -0,0 +1,25 @@
+let colorPickerContainerElement = document.getElementById("colorPickerContainer");
+let spanCodeElement = document.getElementById("selectedColorHexCode");
+
+function bOne() {
+    colorPickerContainerElement.style.backgroundColor = "#e0e0e0";
+    spanCodeElement.textContent = "#e0e0e0";
+}
+
+function bTwo() {
+    colorPickerContainerElement.style.backgroundColor = "#6fcf97";
+    spanCodeElement.textContent = "#6fcf97";
+
+}
+
+function bThree() {
+    colorPickerContainerElement.style.backgroundColor = "#56ccf2";
+    spanCodeElement.textContent = "#56ccf2";
+
+}
+
+function bFour() {
+    colorPickerContainerElement.style.backgroundColor = "#bb6bd9";
+    spanCodeElement.textContent = "#bb6bd9";
+
+}
